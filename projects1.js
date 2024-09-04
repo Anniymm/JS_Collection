5f3cff8e6cf1b5e412dfb8284826dd39461266e9
Anniymm <animaisuradze1999@gmail.com>
2024-09-04 23:14:48 +0400

    add previous tasks

---
+++ projects1.js
@@ -0,0 +1,46 @@
+// 1
+console.log('Hello World!');
+           
+// 2
+let x = 5;
+console.log(x);
+
+// 3
+// eert gamocxadebaze ramdenimec sheidzleba
+let m = 12;
+let n = 18;
+let z = m + n;
+console.log(z);
+
+// 4
+let l = 12, k = 18, j = l - k;
+console.log(j);
+
+// 5
+let g = 12;
+let c = 13;
+console.log(g * c)
+
+// 6
+let p = 4;
+let q = 2;
+let t = p / q;
+console.log(t);
+
+// 7
+let r = 12;
+let w = 23;
+console.log(w % r);
+
+// 9
+let name = 'y', lname = 'x';
+console.log(`i am ${name} from ${lname}.`)    // aq esetebi `` rato? chveulebtivi '' rato ar shveba?
+console.log(name +' '+ lname)  // es ise ra 
+
+// 10
+let str = 'note'.toUpperCase()
+console.log(str)
+
+// 11
+let cvladi = 'raMeEE stringi ROM iyos'.toLowerCase();
+console.log(cvladi)
