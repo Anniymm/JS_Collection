8674aca386b1f202c5f45a21681c40f57bf74716
Anniymm <animaisuradze1999@gmail.com>
2024-09-08 18:29:14 +0400

    add projects for 9 chapter

--- 9thProjects.js
+++ 9thProjects.js
@@ -55,3 +55,65 @@ function passwordValidator() {
 }
 
 console.log(passwordValidator());
+
+// 44
+function countMonthsUntilPaidOff(balance, apr, monthlyPayment) {
+    const monthlyRate = (apr / 100) / 12;
+    // esaa formulao
+    const months = Math.ceil(
+        -Math.log(1 - (monthlyRate * balance) / monthlyPayment) / Math.log(1 + monthlyRate)
+    );
+    return months;
+}
+
+// Example usage
+const result = countMonthsUntilPaidOff(5000, 12, 100);
+console.log(`It will take you ${result} months to pay off this card.`);
+
+
+// 45
+function validation() {
+    let name = prompt('Enter the first name: ');
+    let lname = prompt('Enter the last name:');
+    let zip = prompt('Enter the ZIP code: ');
+    let emp = prompt('Enter an employee ID: ');
+    let isValid = true; // es bolos validaciistvis
+    if (!name) {
+        console.log('The first name must be filled in.');
+        isValid = false;
+    } else if (name.length < 2) {
+        console.log(`${name} is not a valid first name. It is too short.`);
+        isValid = false;
+    }
+    if (!lname) {
+        console.log('The last name must be filled in.');
+        isValid = false;
+    } else if (lname.length < 2) {
+        console.log(`${lname} is not a valid last name. It is too short.`);
+        isValid = false;
+    }
+    const pattern = /^[A-Z]{2}-\d{3}$/;
+    if (!pattern.test(emp)) {
+        console.log(`${emp} is not a valid ID. It should be in the format AA-123.`);
+        isValid = false;
+    }
+    if (isNaN(zip)) {
+        console.log('The ZIP code must be numeric.');
+        isValid = false;
+    }
+    if (isValid) {
+        console.log('Everything is OK.');
+    }
+}
+
+validation();
+
+// 46
+function camel(){
+    let input = prompt('camelCase: ');
+    let arr = input.split(/(?=[A-Z])/);
+    let lowerCaseWords = arr.map(word => word.toLowerCase());
+    let newword = lowerCaseWords.join('_');
+    console.log(`snake_case: ${newword}`)
+}
+camel();
