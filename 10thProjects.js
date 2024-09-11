8a875b3beee07a222c48757454505db88483ec19
Anniymm <animaisuradze1999@gmail.com>
2024-09-11 14:12:12 +0400

    add task for 10

--- 10thProjects.js
+++ 10thProjects.js
@@ -34,4 +34,14 @@ function isAlpha(str) {
   }
   Rate();
 
+  // 51
+  function nested(){
+    for(let i = 0; i <= 12; i++){
+        for(let j = 0; j <= 12; j++){
+            console.log(`${i} X ${j} = ${i*j}`)
+        }
+    }
+}
+nested();
+
 
