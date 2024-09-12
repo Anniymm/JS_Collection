94f3853aa73656f408ec9a92839faed65f9f3627
Anniymm <animaisuradze1999@gmail.com>
2024-09-12 22:22:35 +0400

    add scripts for 11

---
+++ projects11.js
@@ -0,0 +1,18 @@
+// 1
+class Person{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayhello(){
+        console.log('hello');
+    }
+}
+let person1 = new Person('ani', 18);
+console.log(person1)
+console.log(person1.name)
+console.log(person1.age)
+person1.sayhello()
+
+// 2
+
